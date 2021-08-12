bf2ec8fd75ecb5e40995bd1295168ad992142181
Benjamin Richeson <benricheson101@gmail.com>
2021-08-11 21:37:53 -0400

    a change!

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  console.log('[', new Date(), ']', req.method, req.url);
+
   res.writeHead(200);
   res.end('Hello, world!');
 });
